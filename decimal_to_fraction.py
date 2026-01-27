f62d8b741513c0c89fd405b8f595ece5fb93fec4
Femiolajide <131526696+Femiolajide@users.noreply.github.com>
2026-01-27 20:08:22 +0100

    updated

--- decimal_to_fraction.py
+++ decimal_to_fraction.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import fractions as frac
-st.number_input(label="Insert a number",
+deci = st.number_input(label="Insert a number",
                 placeholder="Type a number...",
                 )
+numi = frac.Fraction(deci).limit_denominator().numerator
+deno = frac.Fraction(deci).limit_denominator().denominator
+result = fr"\frac{{{numi}}}{{{deno}}}"
+if numi > deno and numi % deno > 0:
+    mixed_frac = st.toggle("Expressed as mixed fraction")
+    w_num = numi // deno
+    up = numi % deno
+    if mixed_frac:
+        result = fr"\large{{{w_num}}}\frac{{{up}}}{{{deno}}}"
+
+st.latex(result)
