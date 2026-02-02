9a2890407b43e4efddffeadebeb52346e3849e1f
Femiolajide <131526696+Femiolajide@users.noreply.github.com>
2026-02-02 03:02:53 +0100

    Update column_chart.py

--- column_chart.py
+++ column_chart.py
@@ -8,5 +8,6 @@ st.title("Create your bar chart manually")
 st.data_editor(data=pd.DataFrame(columns=["Category","Value"]),
                num_rows="dynamic",
                use_container_width=True,
-               st.column_config=("Category":st.column_config.TextColumn("Category"))
+            #    st.column_config={"Category":st.column_config.TextColumn("Category")
+            
                )
