136038ac62365b31e6d4e6b6c21a60dd7f0939ee
Femiolajide <131526696+Femiolajide@users.noreply.github.com>
2026-02-03 19:00:43 +0100

    updated

---
+++ bar_chart.py
@@ -0,0 +1,285 @@
+import streamlit as st
+import matplotlib.pyplot as plt 
+import textwrap as tw
+import seaborn as sns
+import pandas as pd 
+import random as rd 
+import io
+import datetime as dt
+ts = dt.datetime.now().strftime("%y-%m-%d %H:%M:%S")
+# st.write("---")
+st.title("Create Your Bar Chart Manually")
+st.write("This web application allows you \
+to manually create, customise, and download \
+bar charts.")
+st.write("---")
+tab1, tab3, tab4 = st.tabs(["Data Entry",
+                                 "Default Graph","Edit Graph"])
+with tab1:
+   st.subheader("Start here to enter the data for your bar chart")
+   var = st.text_input("Enter the variable name",
+                    placeholder="Enter the variable name that \
+represents the category being analysed (E.g. Gender, Departments...)")
+   st.write("Enter your data category \
+   and the corresponding frequency in the table below")
+   # st.write("---")
+
+   var = var if var else "Category"
+   # dd = pd.DataFrame(columns=[var,"Frequency"])
+   # st.data_editor(dd)
+
+   data = pd.DataFrame(columns=[var,"Frequency"])
+   data = st.data_editor(
+      data=data,
+      num_rows="dynamic",
+      width="stretch",
+      column_config={var:st.column_config.TextColumn(    
+                     help="Enter your data **category** here!",
+                     required=True
+                  ),
+                  "Frequency":st.column_config.NumberColumn(
+                     help="Enter the **frequency** of the category",
+                     min_value=0,
+                     format="%d",
+                     required=True
+                  )})
+if data[var].duplicated().any():
+   st.error("❌❌❌\nThere is a duplicate in your **category.**  Check your data entry again!")
+if not data.empty and  not data[var].duplicated().any():
+   with tab3:
+      
+      f, a = plt.subplots(figsize=(8,5)) 
+      a.margins(.2)
+      data[var] = [tw.fill(x,10,) for x in data[var]]
+      tot = data["Frequency"].sum()
+      lab_typ = "n%"
+      y_axis = "n"
+      sns.barplot(data=data,
+                  x=var,
+                  y="Frequency",
+                  width=.4,
+                  # height= 5,
+                  # aspect=1.8,
+                  palette="colorblind",
+                  ax=a
+                  
+                  )
+
+      ax = plt.gca()
+      for cont in ax.containers:
+         if lab_typ == "n%":
+            lab = [f"{int(x.get_height())}\n({int(x.get_height())/tot:.1%})" 
+                  
+                  for x in cont]
+            ax.bar_label(container=cont,labels=lab,padding=4,
+                        #  fontsize=12
+                        )
+         elif lab_typ == "%":
+            lab = [f"{int(x.get_height())/tot:.1%}" 
+                  
+                  for x in cont]
+            ax.bar_label(container=cont,labels=lab,padding=4,
+                        #  fontsize=12
+                        )
+         elif lab_typ == "n":
+            lab = [f"{int(x.get_height())}" 
+                  
+                  for x in cont]
+            ax.bar_label(container=cont,labels=lab,padding=4,
+                        #  fontsize=12
+                        )  
+         else:
+            pass
+      plt.xticks(fontsize=10)
+      plt.yticks(fontsize=10)
+      plt.ylabel("\nFrequency\n",fontsize=14,)
+      plt.xlabel("\n"+var+"\n",fontsize=14,)
+      plt.grid(which="both",axis="y",alpha=.3)
+      ax = plt.gca()
+      ax.set_axisbelow(True)
+      plt.title(f"\nFrequency Distribution of {var.capitalize()}",
+                fontsize=16,
+               fontweight="bold",pad=20)
+      sns.despine(ax=a,
+         top=True,
+         right=True,
+         # left=True
+                  # offset=24
+                  )
+      y_tik = ax.get_yticks()
+      if y_axis == "%":
+         plt.yticks(y_tik, [f"{x/tot:.0%}" for x in y_tik])
+      plt.tight_layout()
+      if not data.empty:
+         st.pyplot(f)
+      v_disk = io.BytesIO()
+      plt.savefig(v_disk,format="png")
+      v_disk.seek(0)
+
+      st.download_button(label="Download chart",
+                        data=v_disk,
+                        file_name="bar_chart_"+ts+".png",
+                        mime="image/png")
+
+   with tab4:
+      st.subheader("Edit your graph")
+      with st.expander("Chart Settings",
+                       expanded=True):
+         st.write("Graph size (in inches)")
+         col1, col2 = st.columns(2)
+         with col1:
+            width = st.slider("width",3,30,8)
+         with col2:
+            height = st.slider("Height",3,30,5)            
+         title = st.text_input("Chart title",var)
+         col1, col2, col3, col4 = st.columns(4)
+         with col1:
+            title_size = st.slider("Font size",8,40,16)
+         with col2:
+            title_color = st.color_picker("Colour","#000000")
+         with col3:
+            title_bold = st.checkbox("Bold",True)
+      with st.expander("Axis settings"):
+         x_lab = st.text_input("X-axis Label",var)
+         col1, col2, col3, col4, col5 = st.columns(5)
+         with col1:
+            x_lab_size = st.slider("X label Font size",8,40,11)
+         with col2:
+            x_lab_colour = st.color_picker("X label Colour","#000000")
+         with col3:
+            x_lab_bold = st.checkbox("Bold",False)
+         with col4: 
+            x_lab_dat_size = st.slider("x data Font size",8,40,11)
+         with col5:
+            x_lab_data_colour = st.color_picker("x data Colour","#000000")
+         y_lab = st.text_input("Y-axis Label","Frequency")
+         col1, col2, col3, col4, col5, col6 = st.columns(6)
+         with col1:
+            y_lab_size = st.slider("Y label Font size",8,40,11)
+         with col2:
+            y_lab_colour = st.color_picker("Y label Colour","#000000")
+         with col3:
+            y_lab_bold = st.checkbox("bold",False)
+         with col4: 
+            y_lab_dat_size = st.slider("Y data Font size",8,40,11)
+         with col5:
+            y_lab_data_colour = st.color_picker("Y data Colour","#000000")
+         with col6:
+            y_ax_as_pct = st.toggle("Show y-axis as percentage of total")
+      with st.expander("Data Labels"):
+         col1, col2, col3 = st.columns(3)
+         with col1:
+            data_label = st.selectbox("Select data label format",["None","n","%","n (%)"])
+         with col2:
+            data_label_color = st.color_picker("data label colour","#000000")
+         with col3:
+            data_label_size = st.slider("data label Font size",8,40,11)
+      with st.expander("Bar Options"):
+         col1, col2,col3  = st.columns(3)
+         with col1:
+            bar_width = st.slider("Bar width",.1,1.0,.4)
+         with col2:
+            bar_sort = st.selectbox("bar sort",["None","ascending","descending"])
+            max_cols =3
+         with st.expander("###### ***Pick a color for each of these category***"):
+            cat = data[var].unique()
+            clrs = {}
+            rows = [cat[i:i+max_cols] for i in range(0,len(cat),max_cols)] 
+            for x in rows:
+               cols = st.columns(len(x))
+               for col, kat in zip(cols,x):
+                  with col:
+                     clrs[kat] = st.color_picker(f"{kat}",
+                                          "#2956a3")
+      if bar_sort == "ascending":
+         data = data.sort_values("Frequency",ascending=True)
+      elif bar_sort == "descending":
+         data = data.sort_values("Frequency",ascending=False)
+      else:
+         pass
+      f, a = plt.subplots(figsize=(width,height)) 
+      a.margins(.2)
+      data[var] = [tw.fill(x,12,) for x in data[var]]
+      tot = data["Frequency"].sum()
+      lab_typ = data_label
+      y_axis = y_ax_as_pct
+      sns.barplot(data=data,
+                  x=var,
+                  y="Frequency",
+                  width=bar_width,
+                  # height= 5,
+                  # aspect=1.8,
+                  hue=var,
+                  palette=clrs.values(),
+                  
+                  # palette="colorblind",
+                  ax=a
+                  
+                  )
+
+      ax = plt.gca()
+      for cont in ax.containers:
+         if lab_typ == "n (%)":
+            lab = [f"{int(x.get_height())}\n({int(x.get_height())/tot:.1%})" 
+                  
+                  for x in cont]
+            ax.bar_label(container=cont,labels=lab,padding=4,
+                         fontsize=data_label_size,
+                         color=data_label_color
+                        )
+         elif lab_typ == "%":
+            lab = [f"{int(x.get_height())/tot:.1%}" 
+                  
+                  for x in cont]
+            ax.bar_label(container=cont,labels=lab,padding=4,
+                         fontsize=data_label_size,
+                         color=data_label_color
+                        )
+         elif lab_typ == "n":
+            lab = [f"{int(x.get_height())}" 
+                  
+                  for x in cont]
+            ax.bar_label(container=cont,labels=lab,padding=4,
+                         fontsize=data_label_size,
+                         color=data_label_color
+                        )  
+         else:
+            pass
+      plt.xticks(fontsize=x_lab_dat_size,color=x_lab_data_colour)
+      plt.yticks(fontsize=y_lab_dat_size,
+                 color=y_lab_data_colour)
+      f_wegt_y = "bold" if y_lab_bold else None
+      plt.ylabel("\n"+y_lab,fontsize=y_lab_size,fontweight=f_wegt_y,
+                 color=y_lab_colour)
+      plt.xlabel(x_lab,
+                 color=x_lab_colour,
+                 fontsize=x_lab_size,
+                 fontweight="bold" if x_lab_bold else None,)
+      plt.grid(which="both",axis="y",alpha=.3)
+      ax = plt.gca()
+      ax.set_axisbelow(True)
+      f_wegt_t = "bold" if title_bold else None
+      plt.title(f"\nFrequency Distribution of {var.capitalize()}",
+                fontsize=title_size,
+               fontweight=f_wegt_t,pad=20,
+               color=title_color)
+      sns.despine(ax=a,
+         top=True,
+         right=True,
+         # left=True
+                  # offset=24
+                  )
+      y_tik = ax.get_yticks()
+      if y_ax_as_pct:
+         plt.yticks(y_tik, [f"{x/tot:.0%}" for x in y_tik])
+      plt.tight_layout()
+      if not data.empty:
+         st.pyplot(f)
+      v_disk = io.BytesIO()
+      plt.savefig(v_disk,format="png")
+      v_disk.seek(0)
+
+      st.download_button(label="Download edited chart",
+                        data=v_disk,
+                        file_name="bar_chart_"+ts+".png",
+                        mime="Image/png")
