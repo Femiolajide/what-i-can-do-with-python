619ea81647c9a8570105b840046302e7b043bade
Femiolajide <131526696+Femiolajide@users.noreply.github.com>
2026-02-24 18:26:58 +0100

    Update fraction_calculator.py

--- fraction_calculator.py
+++ fraction_calculator.py
@@ -4,11 +4,11 @@ from fractions import Fraction as fra
 st.title("Fraction Expression Calculator")
 st.write(
     """This calculator evaluates mathematical expressions containing fractions only.
-It accepts valid fraction expressions `(e.g., 3/4 + 5/6)` and returns the answer.
+It accepts valid fraction expressions `(e.g., 3/4 + 5/6)` and returns the answer. 
 Note that :red[exponents] and :red[mixed fractions] are not currently supported."""
 )
 st.write("---")
-st.write(":blue-background[**NOTE:**] Correct answer is guarantted but the proper \
+st.write(":blue-background[**NOTE:**] Correct answer is guarantted, but proper \
 representation of complex expressions is still work-in-progress!")
 def parse_frac_num(x:str):
     x = x.strip()
@@ -52,11 +52,11 @@ if exp:
         else:
             num = fra(ans).limit_denominator().numerator
             den = fra(ans).limit_denominator().denominator
-            if den == num:
+            if abs(den) == abs(num):
                 fn_ans = f"{frac_exp} = {1}"
                 st.latex(fn_ans)
-            elif den < num:
-                if den != 1:
+            elif abs(den) < abs(num):
+                if abs(den) != 1:
                     as_mixed = st.toggle("Express answer as mixed fraction")
                     if as_mixed:
                         w_num = num // den
@@ -67,7 +67,7 @@ if exp:
                         else:
                             fn_ans = fr"{frac_exp} = {num//den}"
                             st.latex(fn_ans)
-                    elif den == 1:
+                    elif abs(den) == 1:
                         fn_ans = fr"{frac_exp} = {num}"
                         st.latex(fn_ans)   
                     else:
