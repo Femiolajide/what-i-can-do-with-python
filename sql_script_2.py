128758461f316dcd283793913966a52b672bc986
Olajide Atanda <131526696+Femiolajide@users.noreply.github.com>
2026-05-12 19:50:19 +0100

    Add files via upload

---
+++ sql_script_2.py
@@ -0,0 +1,202 @@
+import streamlit as st
+import pandas as pd 
+import io 
+import re
+from charset_normalizer import from_bytes
+import csv
+import datetime as dt
+ts = dt.datetime.now().strftime("%y%m%d%H%M%S")
+st.title("CSV/Excel to SQL Table Generator`(PostgreSQL & MYSQL)`")
+st.write("This tool converts CSV and Excel files into ready-to-use **PostgreSQL/MySQL** table creation scripts. It automatically formats column names into snake_case and derives the table name from the uploaded file (also standardised to snake_case).")
+st.markdown("---")
+
+# Function to extract file extension
+def extention(x):
+    import re
+    return re.findall(r"\..+$",x)[0]
+
+def psql_script(df,table_name:str) -> str:
+    def clean_name(txt:str):
+        import re
+        txt = re.sub(r"\s{2,}"," ",txt)
+        txt = re.sub(r"[^a-zA-Z0-9_]","_",txt)
+        for x in txt:
+            if str(x).isupper():
+                txt = txt.replace(x,f"_{x}")
+        txt = txt.strip("_").lower()
+        return txt
+    table_name = clean_name(table_name)
+    def psql_val(df:pd.DataFrame):
+        import pandas as pd
+        import polars as pl
+        df = df.fillna("NULL")
+        df = df.apply(lambda x: x.astype(str))
+        df = df.replace({"":'NULL'})
+        df = df.apply(lambda x: x.str.replace("'","''"))
+        pl_df = pl.DataFrame(df)
+        in_rows = pl_df.rows()
+        temp_txt_list = []
+        for y, x in enumerate(in_rows,1):
+            temp_txt = """("""
+            if y != len(in_rows):
+                for p, q in enumerate(x,1):
+                    if p != len(x) and q != 'NULL':
+                        temp_txt = temp_txt + f"'{q}', "
+                    elif p != len(x) and q == 'NULL':
+                        temp_txt = temp_txt + f"{q}, "
+                    elif p == len(x) and q == 'NULL':
+                        temp_txt = temp_txt + f"{q}),\n"
+                    elif p == len(x):
+                        temp_txt = temp_txt + f"'{q}'),\n"
+                temp_txt_list.append(temp_txt)
+            else:
+                for p, q in enumerate(x,1):
+                    if p != len(x) and q != 'NULL':
+                        temp_txt = temp_txt + f"'{q}',"
+                    elif p != len(x) and q == 'NULL':
+                        temp_txt = temp_txt + f"{q},"
+                    elif p == len(x) and q == 'NULL':
+                        temp_txt = temp_txt + f"{q})"
+                    elif p == len(x):
+                        temp_txt = temp_txt + f"'{q}')"
+                temp_txt_list.append(temp_txt)
+        return  "VALUES " + """""".join(temp_txt_list)
+
+    def psql_create(df,table_name:str):
+        from pandas.io.sql import get_schema
+        import re
+        df = df.rename(columns=lambda x: clean_name(x))
+        df = df.apply(lambda x: x.astype(str))
+        script = get_schema(df,table_name) + ";\n\n"
+        script_2 = re.sub(r"^CREATE TABLE","INSERT INTO",script)
+        script_2 = re.sub(r' [A-Z]+(?=,)| [A-Z]+(?=\n\);)',"",script_2).rstrip("\n;") + "\n\n"
+        script_update = script.replace("CREATE TABLE",
+                                       "CREATE TABLE IF NOT EXISTS")
+        return script_update + script_2
+    return psql_create(df,table_name) + psql_val(df)
+
+
+# for mysql script 
+
+def mysql_script(df,table_name:str) -> str:
+    def clean_name(txt:str):
+        import re
+        txt = re.sub(r"\s{2,}"," ",txt)
+        txt = re.sub(r"[^a-zA-Z0-9_]","_",txt)
+        for x in txt:
+            if str(x).isupper():
+                txt = txt.replace(x,f"_{x}")
+        txt = txt.strip("_").lower()
+        return txt
+    table_name = clean_name(table_name)
+    def mysql_val(df:pd.DataFrame):
+        import pandas as pd
+        import polars as pl
+        df = df.fillna("NULL")
+        df = df.apply(lambda x: x.astype(str))
+        df = df.replace({"":'NULL'})
+        df = df.apply(lambda x: x.str.replace("'","\\'"))
+        pl_df = pl.DataFrame(df)
+        in_rows = pl_df.rows()
+        temp_txt_list = []
+        for y, x in enumerate(in_rows,1):
+            temp_txt = """("""
+            if y != len(in_rows):
+                for p, q in enumerate(x,1):
+                    if p != len(x) and q != 'NULL':
+                        temp_txt = temp_txt + f"'{q}', "
+                    elif p != len(x) and q == 'NULL':
+                        temp_txt = temp_txt + f"{q}, "
+                    elif p == len(x) and q == 'NULL':
+                        temp_txt = temp_txt + f"{q}),\n"
+                    elif p == len(x):
+                        temp_txt = temp_txt + f"'{q}'),\n"
+                temp_txt_list.append(temp_txt)
+            else:
+                for p, q in enumerate(x,1):
+                    if p != len(x) and q != 'NULL':
+                        temp_txt = temp_txt + f"'{q}',"
+                    elif p != len(x) and q == 'NULL':
+                        temp_txt = temp_txt + f"{q},"
+                    elif p == len(x) and q == 'NULL':
+                        temp_txt = temp_txt + f"{q})"
+                    elif p == len(x):
+                        temp_txt = temp_txt + f"'{q}')"
+                temp_txt_list.append(temp_txt)
+        return  "VALUES " + """""".join(temp_txt_list)
+
+    def mysql_create(df:pd.DataFrame,table_name:str):
+        from pandas.io.sql import get_schema
+        import re
+        df = df.rename(columns=lambda x: clean_name(x))
+        df = df.apply(lambda x: x.astype(str))
+        script = get_schema(df,table_name).replace('"','`') + ";\n\n"
+        script_2 = re.sub(r"^CREATE TABLE","INSERT INTO",script)
+        script_2 = re.sub(r' [A-Z]+(?=,)| [A-Z]+(?=\n\);)',"",script_2).rstrip("\n;") + "\n\n"
+        script_update = script.replace("CREATE TABLE",
+                                       "CREATE TABLE IF NOT EXISTS")
+        return script_update + script_2
+    return mysql_create(df,table_name) + mysql_val(df)
+
+
+
+
+
+encod = "utf-8"
+data = ""
+
+file = st.file_uploader("**Upload your data**",
+                        help="Only csv, txt, tsv and excel file are supported"
+                        )
+
+if file:
+    if re.search(r"\.tsv$|\.csv$|\.txt$|\.xlsx$|\.xls$",file.name):
+        table_name = re.sub(r"\.tsv$|\.csv$|\.txt$|\.xlsx$|\.xls$","",file.name)
+        if re.search(r"\.tsv$|\.csv$|\.txt$",file.name):
+            raw = file.getvalue()
+            encod = from_bytes(raw).best().encoding
+            sample_txt = raw.decode(encoding="utf-8",errors="ignore")
+            delimiter = csv.Sniffer().sniff(sample_txt).delimiter
+            if delimiter.strip() and delimiter != '\t':
+                try:
+                    data = pd.read_csv(file,encoding=encod,sep=delimiter,na_filter=False)
+                except:
+                    st.error("ERROR! Check your file again, does it have proper table structure?")
+            else:
+                try:
+                    data = pd.read_csv(file,encoding=encod,sep="\t",na_filter=False)
+                except:
+                    st.error("ERROR! Check your file again, does it have proper table structure?")
+        else:
+            sheets = [*pd.read_excel(file,sheet_name=None).keys()]
+            if len(sheets) == 1:
+                data = pd.read_excel(file,na_filter=False)
+            else:
+                sheet = st.selectbox("### Select the sheet you want to pick data from",
+                            sheets)
+                data = pd.read_excel(file, sheet_name=sheet,
+                                     na_filter=False)
+    else:
+        st.error(f"ERROR: You can only upload csv, txt, tsv or excel file NOT '{extention(file.name)}' file")
+    if not isinstance(data,str):
+        with st.expander("Preview data"):
+            st.dataframe(data.head())
+            st.write(f"The data you uploaded has `{data.shape[0]:,}` row(s) and `{data.shape[1]:,}` column(s)")
+        dbms = st.selectbox("Choose a database management system",
+                     ["PostgreSQL","MySQL"])
+        if dbms == "PostgreSQL":
+            data_table = psql_script(data,table_name)
+            data_table = """SET client_encoding = 'UTF8';\n\n""" + data_table
+            st.download_button(
+                "Download as **`postgresql`** data script",
+                data=data_table.encode("utf-8",errors="replace").decode("utf-8"),
+                mime="text/plain",
+                file_name=f"psql_script_{ts}.sql")
+        else:
+            data_table = mysql_script(data,table_name)
+            data_table = """SET NAMES utf8mb4;\n\n""" + data_table
+            st.download_button(
+                "Download as **`mysql`** data script",
+                data=data_table.encode("utf-8",errors="replace").decode("utf-8"),
+                mime="text/plain",
+                file_name=f"mysql_script_{ts}.sql")    
